2d1e534af232eff95eabb07bac84acba4f7e004b
Alexander <alexander.advanced@gmail.com>
2023-11-06 17:51:59 +0100

    update

---
+++ src/Form/Type/ChoiceSearchableType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AlexanderA2\SymfonyBasics\Form\Type;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChoiceSearchableType extends AbstractType
+{
+    public function __construct(
+        protected ParameterBagInterface $parameters,
+    ) {
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr']['data-search-picker'] = '1';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'widget' => 'single_text',
+            'html5' => false,
+        ]);
+    }
+
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+}
